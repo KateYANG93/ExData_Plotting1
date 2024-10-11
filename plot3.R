a375a80c9a301d29af3001985a521634ad3e0825
KateYANG93 <35400165+KateYANG93@users.noreply.github.com>
2024-10-10 22:17:01 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -55,7 +55,6 @@ x_labels <- as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03"))
 axis(1, at = x_labels, labels = weekdays_labels, las = 1)
 
 # Add legend
-# Get the current plot's x and y limits to help position the legend, 
 legend("topright",
        legend=c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"),lty = 1,,col=c("black","red","blue"),
        text.width = 51399 ) # Increase the width of the text area, making the box wider.Since in png format the text is out of legend box
